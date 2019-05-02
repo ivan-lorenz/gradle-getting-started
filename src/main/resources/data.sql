5ebb742ca49656d033fefc7f7979405ed7c0705c
Andre Lopes <andre.lopes@glovoapp.com>
2019-05-03 01:12:15 +0200

    Finish implementing events endpoint step 20

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,8 @@ insert into event (id, name, description, venue_id, start, category, url_image) 
 insert into event (id, name, description, venue_id, start, category, url_image) values(3, 'La Liga game', 'Madrid football game', 1, ' 2019-01-02 22:00:00','FOOTBALL_MATCH', 'https://images-na.ssl-images-amazon.com/images/I/61BWocKFkeL._SX425_.jpg');
 
 insert into ticket_config (id, type, price, quantity, event_id) values(4, 'VIP', 100, 1000, 2);
-insert into ticket_config (id, type, price, quantity, event_id) values(5, 'NORMAL', 50, 2000, 2);
+insert into ticket_config (id, type, price, quantity, event_id) values(5, 'NORMAL', 50, 1, 2);
+
 insert into ticket_config (id, type, price, quantity, event_id) values(6, 'VIP', 100, 2000, 3);
 insert into ticket_config (id, type, price, quantity, event_id) values(7, 'NORMAL', 50, 3000, 3);
 
