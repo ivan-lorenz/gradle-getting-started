1bee91f90456915409cd02b322824a4f5ce1a393
Andre Lopes <andre.lopes@glovoapp.com>
2019-05-03 12:04:43 +0200

    Finish implementing events endpoint step 35

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,8 +16,8 @@ insert into ticket (id, type, customer_id, event_id, seat_zone_url) values(8, 'N
 insert into store (id, name, zone_id, venue_id, url_image) values(3603,' Barca Burger', '4D', 1, 'https://www.ishn.com/ext/resources/Issues/2016/September/columns/ISHN_0916_C1_pic.jpg?1472749174');
 insert into store (id, name, zone_id, venue_id, url_image) values(3606, 'Pho Viet', '5D', 1, 'https://pbs.twimg.com/profile_images/2094778704/Kings_Logo_CMYK__1_.jpg');
 
-insert into product (id, name, price, store_id, url_image) values(12, 'Burger', 10, 9,'https://res.cloudinary.com/hksqkdlah/image/upload/s--or4BUPPr--/c_scale,dpr_2.0,f_auto,h_344,q_auto:low,w_344/24811_sfs-black-bean-burger-50');
-insert into product (id, name, price, store_id, url_image) values(13, 'Coca-cola', 5, 9, 'https://images-na.ssl-images-amazon.com/images/I/81mEIp4PMBL._SL1500_.jpg');
-insert into product (id, name, price, store_id, url_image) values(14, 'Fish&Chips', 20, 10, 'https://d3cizcpymoenau.cloudfront.net/images/37243/SFS_Fish_and_Chips-35.jpg');
-insert into product (id, name, price, store_id, url_image) values(15, 'Salad', 10, 11, 'https://www.tasteofhome.com/wp-content/uploads/2017/10/exps6498_MRR133247D07_30_5b_WEB-2.jpg');
-insert into product (id, name, price, store_id, url_image) values(16, 'Water', 5, 11,'https://slowdancejournal.files.wordpress.com/2015/11/plastic-water-bottle.jpg');
+insert into product (id, name, price, store_id, url_image) values(12, 'Burger', 10,3603 ,'https://res.cloudinary.com/hksqkdlah/image/upload/s--or4BUPPr--/c_scale,dpr_2.0,f_auto,h_344,q_auto:low,w_344/24811_sfs-black-bean-burger-50');
+insert into product (id, name, price, store_id, url_image) values(13, 'Coca-cola', 5, 3603, 'https://images-na.ssl-images-amazon.com/images/I/81mEIp4PMBL._SL1500_.jpg');
+insert into product (id, name, price, store_id, url_image) values(14, 'Fish&Chips', 20, 3606, 'https://d3cizcpymoenau.cloudfront.net/images/37243/SFS_Fish_and_Chips-35.jpg');
+insert into product (id, name, price, store_id, url_image) values(15, 'Salad', 10, 3606, 'https://www.tasteofhome.com/wp-content/uploads/2017/10/exps6498_MRR133247D07_30_5b_WEB-2.jpg');
+insert into product (id, name, price, store_id, url_image) values(16, 'Water', 5, 3606,'https://slowdancejournal.files.wordpress.com/2015/11/plastic-water-bottle.jpg');
